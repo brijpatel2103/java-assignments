51d6b50a608509f8895717b970aa1376b409be5a
brijpatel2103 <135827242+brijpatel2103@users.noreply.github.com>
2023-07-31 11:07:59 +0530

    31/7/23

--- TestNGDemo/src/com/framework/HybridFW.java
+++ TestNGDemo/src/com/framework/HybridFW.java
@@ -8,12 +8,16 @@ import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class HybridFW {
-	@Test
-	public String[][] readExcel() throws Exception, IOException {
-		String[][] data = null;
+	@DataProvider(name = "brijesh")
+	public Object[][] readExcel() throws Exception, IOException {
+		Object[][] data = null;
 
 		String filepath = "F:\\AUTOMATION\\Selenium_Practice.xlsx";
 		File file = new File(filepath);
@@ -21,12 +25,12 @@ public class HybridFW {
 		Sheet sheet = workbook.getSheet("Sheet3");
 		int rows = sheet.getPhysicalNumberOfRows();
 		System.out.println("Total Rows : " + rows);
-		data = new String[rows][];
+		data = new Object[rows][];
 		for (int i = 0; i < data.length; i++) {
 			Row row = sheet.getRow(i);
 			int cols = row.getPhysicalNumberOfCells();
 			System.out.println("Total Cols : " + cols);
-			data[i] = new String[cols];
+			data[i] = new Object[cols];
 			for (int j = 0; j < data[i].length; j++) {
 				Cell cell = row.getCell(j);
 				cell.setCellType(CellType.STRING);
@@ -34,5 +38,42 @@ public class HybridFW {
 			}
 		}
 		return data;
-  }
+	}
+	WebDriver driver = null;
+	@Test(dataProvider = "brijesh")
+	public void test(String keyword, String data) throws Exception
+	{
+		System.setProperty("webdriver.chrome.driver", "F:\\AUTOMATION\\selenium librari\\chromedriver.exe");
+		if(keyword.equals("open browser"))
+		{
+			driver=new ChromeDriver();
+			Thread.sleep(2000);
+		}
+		else if(keyword.equals("enter url"))
+		{
+			driver.get(data);
+			driver.manage().window().maximize();
+			Thread.sleep(2000);
+		}
+		else if(keyword.equals("enter username"))
+		{
+			driver.findElement(By.name("user-name")).sendKeys(data);
+			Thread.sleep(2000);
+		}
+		else if(keyword.equals("enter password"))
+		{
+			driver.findElement(By.name("password")).sendKeys(data);
+			Thread.sleep(2000);
+		}
+		else if(keyword.equals("click login"))
+		{
+			driver.findElement(By.name("login-button")).click();
+			Thread.sleep(2000);
+		}
+		else if(keyword.equals("close browser"))
+		{
+			driver.close();
+		}
+	}
+	
 }
